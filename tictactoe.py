971ee79b8c78beabd6f20fe7d2f00fe75fb70355
redzebra42 <delaigletristan42@gmail.com>
2025-12-05 14:53:07 +0100

    Add basic tictactoe class

---
+++ tictactoe.py
@@ -0,0 +1,60 @@
+import numpy as np
+
+
+class TicTacToe():
+    
+    def __init__(self, size):
+        self.size = size
+        self.board = np.array([[0 for _ in range(size)] for _ in range(size)])
+        self.current_player = 1
+        self.winner = 0
+    
+    def is_over(self):
+        return self.winner > 0
+
+    def is_legal(self, i, j):
+        return self.board[i, j] == 0
+    
+    def check_victory(self, i, j):
+        
+        # vertical check
+        if self.board[:, j] == [self.current_player for _ in range(self.size)]:
+            self.winner = self.current_player
+            return True
+        
+        # horizontal check
+        if self.board[i, :] == [self.current_player for _ in range(self.size)]:
+            self.winner = self.current_player
+            return True
+        
+        # diagonal check
+        if i == j or i == self.size - j - 1:
+            
+            # first diagonal
+            tmp = True
+            for k in range(self.size):
+                if self.board[k, k] != self.current_player:
+                    tmp = False
+                    break
+            if tmp:
+                self.winner = self.current_player
+                return True
+            
+            # second diagonal
+            tmp = True
+            for k in range(self.size):
+                if self.board[k, k] != self.current_player:
+                    tmp = False
+                    break
+            if tmp:
+                self.winner = self.current_player
+                return True
+        
+        return False
+
+
+    def play_at(self, i, j):
+        if self.is_legal(i, j):
+            self.board[i, j] = self.current_player
+            self.check_victory(i, j)
+            self.current_player = 3 - self.current_player
