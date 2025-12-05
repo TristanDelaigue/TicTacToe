442bd88ac38efb6bc6ac2eea32347ccd4d0bd3fd
redzebra42 <delaigletristan42@gmail.com>
2025-12-05 15:44:52 +0100

    Add is_over test

--- test_tictactoe.py
+++ test_tictactoe.py
@@ -12,3 +12,16 @@ def test_is_legal():
     test_game.play_at(1, 1)
     assert not test_game.is_legal(1, 1)
 
+def test_is_over():
+    test_game = TicTacToe(3)
+    test_game.play_at(0, 0)
+    assert not test_game.is_over()
+    test_game.play_at(1, 1)
+    assert not test_game.is_over()
+    test_game.play_at(0, 1)
+    assert not test_game.is_over()
+    test_game.play_at(2, 2)
+    assert not test_game.is_over()
+    test_game.play_at(0, 2)
+    assert test_game.is_over()
+
